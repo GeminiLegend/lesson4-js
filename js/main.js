c343dbdc84153f737d02e389383795503071f858
Marius Talpos <andreitalpos@yahoo.com>
2016-01-12 19:20:56 -0500

    prototype inheritance

--- js/main.js
+++ js/main.js
@@ -13,43 +13,100 @@ each animal should have a name, type, and make a sound.
 // code to interface
 
 window.Zoo = {}
-Zoo.animalTypes = [ "Bears", "Lions", "Elephants", "Monkeys" ];
+Zoo.animalTypes = [ "Bear", "Lion", "Elephant", "Monkey" ];
 Zoo.animals = [];
 
-//constructor functions
-Zoo.Bears = function (name, type, sound){
+//Animal Class aka constructor function aka Super Class
+Zoo.Animal = function(name, sound){
 	this.name = name;
-	this.type = type;
+	this.sound = sound;
+};
+
+//Child of Animal aka Sub Class
+Zoo.Bear = function(name, sound){
+	Zoo.Animal.call(this, name, sound); //Calling the constructor of the parent
+	
+	this.type = 'Bear'
 	this.sound = sound;
 
 	Zoo.animals.push(this);
 };
 
-Zoo.Lions = function (name, type, sound){
-	this.name = name;
-	this.type = type;
-	this.sound = sound;
-
-	Zoo.animals.push(this);
-};	
-
-Zoo.Elephants = function (name, type, sound){
-	this.name = name;
-	this.type = type;
+Zoo.Lion = function(name, sound){
+	Zoo.Animal.call(this, name, sound); //Calling the constructor of the parent
+	
+	this.type = 'Lion'
 	this.sound = sound;
 
 	Zoo.animals.push(this);
 };
 
-Zoo.Monkeys = function (name, type, sound){
-	this.name = name;
-	this.type = type;
+Zoo.Elephant = function(name, sound){
+	Zoo.Animal.call(this, name, sound); //Calling the constructor of the parent
+	
+	this.type = 'Elephant'
 	this.sound = sound;
 
 	Zoo.animals.push(this);
 };
 
-Zoo.getAnimalCountByType = function (type, animals){
+Zoo.Monkey = function(name, sound){
+	Zoo.Animal.call(this, name, sound); //Calling the constructor of the parent
+	
+	this.type = 'Monkey'
+	this.sound = sound;
+
+	Zoo.animals.push(this);
+};
+//Linking the prototype chain
+Zoo.Bear.prototype = new Zoo.Animal();
+
+Zoo.Lion.prototype = new Zoo.Animal();
+
+Zoo.Elephant.prototype = new Zoo.Animal();
+
+Zoo.Monkey.prototype = new Zoo.Animal();
+
+
+Zoo.Animal.prototype.countByType = function ( animals){
+	var count = 0;
+	var type = this.type;
+
+	for (var i = 0; i < Zoo.animals.length; i++) {
+		var animal = animals[i];
+		if(animal.type == type) count++;
+	}; 
+		var nameOfType = type.toLowerCase() + 'Count: ';
+	console.log(nameOfType, count);
+	return count;
+};
+
+//Creating an instance of the Bear class
+var rufus = new Zoo.Bear('Rufus', 'GrrrrBaaahhhhh');
+
+var dufus = new Zoo.Bear('Dufus', 'GrrrrBaaahhhhh');
+
+var leo = new Zoo.Lion('Leo', 'Roar')
+
+var lio = new Zoo.Lion('Lio', 'Roar')
+
+var dumbo = new Zoo.Elephant('Dumbo', 'Something')
+
+var dimbo = new Zoo.Elephant('Dimbo', 'Something')
+
+var abu = new Zoo.Monkey('Abu', 'Ooh')
+
+var apu = new Zoo.Monkey('Apu', 'Ooh')
+
+//Extending the prototype of the parent
+Zoo.Animal.prototype.makeIntroduction = function(){
+	console.log('Hello my name is ' + this.name);
+}
+
+
+
+//Hwk: Make this an instance method of all child instances so that you can ommit type from the argument list
+/*Zoo.getAnimalCountByType = function (type, animals){
 	var count = 0;
 
 	for (var i = 0; i < Zoo.animals.length; i++) {
@@ -60,44 +117,6 @@ Zoo.getAnimalCountByType = function (type, animals){
 	var name = type.toLowerCase() + 'Count: ';
 	console.log(name, count);
 	return count;
-	/*
-	1. Refer animal to a type
-		if(animal.type = type) console.log( type, count)
-	2. Function counts objects with same type property
-	3. Console displays number of objects by type with name 
-	*/
 }
-
-
-//Individual animals
-var Joe = new Zoo.Bears( 'Joe',  'Bears', 'Grr');
-
-var Bob = new Zoo.Bears( 'Bob', 'Bears', 'Grr');
-
-var Leo = new Zoo.Lions( 'Leo', 'Lions', 'Roar');
-
-var Lio = new Zoo.Lions( 'Lio', 'Lions', 'Roar');
-
-var Dumbo = new Zoo.Elephants( 'Dumbo', 'Elephants', 'Something');
-
-var Dimbo = new Zoo.Elephants( 'Dimbo', 'Elephants', 'Something');
-
-var Abu = new Zoo.Monkeys( 'Abu', 'Monkeys', 'Ooh');
-
-var Apu = new Zoo.Monkeys( 'Apu', 'Monkeys', 'Ooh');
-
-
-Zoo.getAnimalCountByType('Bears', Zoo.animals);
-/*
-- Make the count method log the count out automatically
-
-- It should make the name of the thing to be counted dynamically
-with exactly the same capitalization and all.
 */
-
-var lionCount = Zoo.getAnimalCountByType('Lions', Zoo.animals);
-
-var elephantCount = Zoo.getAnimalCountByType('Elephants', Zoo.animals);
-
-var monkeyCount = Zoo.getAnimalCountByType('Monkeys', Zoo.animals);
 
